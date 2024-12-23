87cc4b6b13b4bedd59c2e77c131a552576472fe8
WAYAN MURDIONI <154336943+jaka2m@users.noreply.github.com>
2024-12-23 14:57:50 +0800

    Add files via upload

--- subworker.js
+++ subworker.js
@@ -25,7 +25,7 @@ async function handleRequest(request) {
 
     try {
         // Mengambil data proxy
-        const response = await fetch('https://geol.biz.id/proxy_list.txt');
+        const response = await fetch('https://geol.biz.id/bot/proxy_list.txt');
         if (!response.ok) throw new Error('Failed to load proxy list.');
         const text = await response.text();
         const proxies = text.split('\n').filter(proxy => proxy.trim() !== '');
@@ -54,37 +54,37 @@ async function handleRequest(request) {
                 // sub1
                 if (type === 'vless') {
                     if (wildcard) {
-                        urlString = `vless://${uuid}@${wildcard}:443?encryption=none&type=ws&host=${wildcard}.tp1.bmkg.xyz&path=%2F${ip}-${port}&security=tls&sni=${wildcard}.tp1.bmkg.xyz#${country}+${provider}`;
+                        urlString = `vless://${uuid}@${wildcard}:443?encryption=none&type=ws&host=${wildcard}.sub.gpj.us.kg&path=%2F${ip}-${port}&security=tls&sni=${wildcard}.sub.gpj.us.kg#${country}+${provider}`;
                     } else {
-                        urlString = `vless://${uuid}@tp1.bmkg.xyz:443?encryption=none&type=ws&host=tp1.bmkg.xyz&path=%2F${ip}-${port}&security=tls&sni=tp1.bmkg.xyz#${country}+${provider}`;
+                        urlString = `vless://${uuid}@sub.gpj.us.kg:443?encryption=none&type=ws&host=sub.gpj.us.kg&path=%2F${ip}-${port}&security=tls&sni=sub.gpj.us.kg#${country}+${provider}`;
                     }
                 } else if (type === 'trojan') {
                     if (wildcard) {
-                        urlString = `trojan://${uuid}@${wildcard}:443?encryption=none&type=ws&host=${wildcard}.tp1.bmkg.xyz&path=%2F${ip}-${port}&security=tls&sni=${wildcard}.tp1.bmkg.xyz#${country}+${provider}`;
+                        urlString = `trojan://${uuid}@${wildcard}:443?encryption=none&type=ws&host=${wildcard}.sub.gpj.us.kg&path=%2F${ip}-${port}&security=tls&sni=${wildcard}.sub.gpj.us.kg#${country}+${provider}`;
                     } else {
-                        urlString = `trojan://${uuid}@tp1.bmkg.xyz:443?encryption=none&type=ws&host=tp1.bmkg.xyz&path=%2F${ip}-${port}&security=tls&sni=tp1.bmkg.xyz#${country}+${provider}`;
+                        urlString = `trojan://${uuid}@sub.gpj.us.kg:443?encryption=none&type=ws&host=sub.gpj.us.kg&path=%2F${ip}-${port}&security=tls&sni=sub.gpj.us.kg#${country}+${provider}`;
                     }
                 } else if (type === 'ss') {
                     const ssBase64 = btoa('onEto5d2d2a2c5Z3b2E9sbLAAe6bYmD3v8l8fGVbt34='); // Base64 encode SS password (change with your actual password)
                     if (wildcard) {
-                        urlString = `ss://bm9uZTo1ZDJlYmQyYS05Y2I5LTRkMWItYWY1NS04NjE3ZDNlODFmMzk%3D@${wildcard}:443?encryption=none&type=ws&host=${wildcard}.tp1.bmkg.xyz&path=%2F${ip}-${port}&security=tls&sni=${wildcard}.tp1.bmkg.xyz#${country}+${provider}`;
+                        urlString = `ss://bm9uZTo1ZDJlYmQyYS05Y2I5LTRkMWItYWY1NS04NjE3ZDNlODFmMzk%3D@${wildcard}:443?encryption=none&type=ws&host=${wildcard}.sub.gpj.us.kg&path=%2F${ip}-${port}&security=tls&sni=${wildcard}.sub.gpj.us.kg#${country}+${provider}`;
                     } else {
-                        urlString = `ss://bm9uZTo1ZDJlYmQyYS05Y2I5LTRkMWItYWY1NS04NjE3ZDNlODFmMzk%3D@tp1.bmkg.xyz:443?encryption=none&type=ws&host=tp1.bmkg.xyz&path=%2F${ip}-${port}&security=tls&sni=tp1.bmkg.xyz#${country}+${provider}`;
+                        urlString = `ss://bm9uZTo1ZDJlYmQyYS05Y2I5LTRkMWItYWY1NS04NjE3ZDNlODFmMzk%3D@sub.gpj.us.kg:443?encryption=none&type=ws&host=sub.gpj.us.kg&path=%2F${ip}-${port}&security=tls&sni=sub.gpj.us.kg#${country}+${provider}`;
                     }
                 }
             } else if (countryParam2) {
                 // sub2
                 if (type === 'vless') {
                     if (wildcard) {
-                        urlString = `vless://FREE-V2RAY-BMKG-XYZ@${wildcard}:443?encryption=none&type=ws&host=${wildcard}.xvp.bmkg.xyz&path=%2Fvl%3D${ip}%3A${port}&security=tls&sni=${wildcard}.xvp.bmkg.xyz#${country}+${provider}`;
+                        urlString = `vless://GEO-PROJECT@${wildcard}:443?encryption=none&type=ws&host=${wildcard}.xvp.bmkg.xyz&path=%2Fvl%3D${ip}%3A${port}&security=tls&sni=${wildcard}.xvp.bmkg.xyz#${country}+${provider}`;
                     } else {
-                        urlString = `vless://FREE-V2RAY-BMKG-XYZ@xvp.bmkg.xyz:443?encryption=none&type=ws&host=xvp.bmkg.xyz&path=%2Fvl%3D${ip}%3A${port}&security=tls&sni=xvp.bmkg.xyz#${country}+${provider}`;
+                        urlString = `vless://GEO-PROJECT@xvp.bmkg.xyz:443?encryption=none&type=ws&host=xvp.bmkg.xyz&path=%2Fvl%3D${ip}%3A${port}&security=tls&sni=xvp.bmkg.xyz#${country}+${provider}`;
                     }
                 } else if (type === 'trojan') {
                     if (wildcard) {
-                        urlString = `trojan://FREE-V2RAY-BMKG-XYZ@${wildcard}:443?encryption=none&type=ws&host=${wildcard}.xvp.bmkg.xyz&path=%2Ftr%3D${ip}%3A${port}&security=tls&sni=${wildcard}.xvp.bmkg.xyz#${country}+${provider}`;
+                        urlString = `trojan://GEO-PROJECT@${wildcard}:443?encryption=none&type=ws&host=${wildcard}.xvp.bmkg.xyz&path=%2Ftr%3D${ip}%3A${port}&security=tls&sni=${wildcard}.xvp.bmkg.xyz#${country}+${provider}`;
                     } else {
-                        urlString = `trojan://FREE-V2RAY-BMKG-XYZ@xvp.bmkg.xyz:443?encryption=none&type=ws&host=xvp.bmkg.xyz&path=%2Ftr%3D${ip}%3A${port}&security=tls&sni=xvp.bmkg.xyz#${country}+${provider}`;
+                        urlString = `trojan://GEO-PROJECT@xvp.bmkg.xyz:443?encryption=none&type=ws&host=xvp.bmkg.xyz&path=%2Ftr%3D${ip}%3A${port}&security=tls&sni=xvp.bmkg.xyz#${country}+${provider}`;
                     }
                 }
             }
